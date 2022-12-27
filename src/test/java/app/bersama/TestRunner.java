4374186f74b45e867e47c875b0b234ec4c7a42e2
HaryUK29 <haryutamakurniawan@gmail.com>
2022-12-27 09:39:46 +0700

    final commit

--- src/test/java/app/bersama/TestRunner.java
+++ src/test/java/app/bersama/TestRunner.java
@@ -18,7 +18,7 @@ import java.time.Duration;
  * @project java-cucumber-learning
  */
 
-@CucumberOptions(features = "src/test/java/app/bersama/features",
+@CucumberOptions(features = "src/test/java/app/bersama/features/BuyProduct.feature",
         glue = "app.bersama.steps",
         tags = "",
         plugin = {
